27ffeccbff6ff4d2e71aa8a7a7981413d6ce0fba
Rafael Souza <rafa.nds13@gmail.com>
2023-12-23 18:56:10 -0300

    Adicionando as interfaces DepartmentDao e SellerDao

---
+++ src/interfaces/DepartmentDao.java
@@ -0,0 +1,13 @@
+package interfaces;
+
+import java.util.List;
+
+import model.Department;
+
+public interface DepartmentDao {
+    void insert(Department obj);
+    void update(Department obj);
+    void deleteById(Integer id);
+    Department findById(Integer id);
+    List<Department> findAll();
+}
---
+++ src/interfaces/SellerDao.java
@@ -0,0 +1,12 @@
+package interfaces;
+
+import java.util.List;
+import model.Seller;
+
+public interface SellerDao {
+    void insert(Seller obj);
+    void update(Seller obj);
+    void deleteById(Integer id);
+    Seller findById(Integer id);
+    List<Seller> findAll();
+}
